290ff610fb3feb6a914229db09cf6a46b3df8aa0
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-13 16:09:29 +0300

    Add SQLAlchemy

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,22 @@
+# Tuodaan Flask käyttöön
 from flask import Flask
 app = Flask(__name__)
 
+# Tuodaan SQLAlchemy käyttöön
+from flask_sqlalchemy import SQLAlchemy
+# Käytetään birds.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
+# kertoo, että tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
+# samassa paikassa
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///birds.db"
+# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
+app.config["SQLALCHEMY_ECHO"] = True
+
+# Luodaan db-olio, jota käytetään tietokannan käsittelyyn
+db = SQLAlchemy(app)
+
+# Luetaan kansiosta application tiedoston views sisältö
 from application import views
+
+
+# Luodaan lopulta tarvittavat tietokantataulut
+db.create_all()
