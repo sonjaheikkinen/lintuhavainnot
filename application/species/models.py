6aa96e691ad3af17ff534315c03a3e7ff926ce28
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-13 16:51:03 +0300

    Modify column names in models.py

--- application/species/models.py
+++ application/species/models.py
@@ -4,15 +4,15 @@ class Species(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(144), nullable=False)
     species = db.Column(db.String(144), nullable=False)
-    genus = db.Column(db.String(144), nullable=False)
-    family = db.Column(db.String(144), nullable=False)
-    order = db.Column(db.String(144), nullable=False)
+    spGenus = db.Column(db.String(144), nullable=False)
+    spFamily = db.Column(db.String(144), nullable=False)
+    spOrder = db.Column(db.String(144), nullable=False)
     description = db.Column(db.String(144), nullable=True)
 
-    def __init__(self, name, species, genus, family, order, description):
+    def __init__(self, name, species, spGenus, spFamily, spOrder, description):
         self.name = name
         self.species = species
-        self.genus = genus
-        self.family = family
-        self.order = order
+        self.spGenus = spGenus
+        self.spFamily = spFamily
+        self.spOrder = spOrder
         self.description = description
