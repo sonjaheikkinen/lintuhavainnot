41b1ece6dc90ceb8a76aab5c38cea766f7c9394e
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-13 16:27:28 +0300

    Add species model in models.py

---
+++ application/species/models.py
@@ -0,0 +1,18 @@
+from application import db
+
+class Species(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(144), nullable=False)
+    species = db.Column(db.String(144), nullable=False)
+    genus = db.Column(db.String(144), nullable=False)
+    family = db.Column(db.String(144), nullable=False)
+    order = db.Column(db.String(144), nullable=False)
+    description = db.Column(db.String(144), nullable=True)
+
+    def __init__(self, name, species, genus, family, order, description):
+        self.name = name
+        self.species = species
+	self.genus = genus
+	self.family = family
+	self.order = order
+	self.description = description
