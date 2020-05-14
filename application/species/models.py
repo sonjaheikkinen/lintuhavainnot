671d0eab763c2a062fcc5978b68fe206001aa5ad
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-14 16:24:20 +0300

    Comment out attributes from models.py for testing purposes

--- application/species/models.py
+++ application/species/models.py
@@ -3,16 +3,17 @@ from application import db
 class Species(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(144), nullable=False)
-    species = db.Column(db.String(144), nullable=False)
-    sp_genus = db.Column(db.String(144), nullable=False)
-    sp_family = db.Column(db.String(144), nullable=False)
-    sp_order = db.Column(db.String(144), nullable=False)
-    description = db.Column(db.String(144), nullable=True)
+    #species = db.Column(db.String(144), nullable=False)
+    #sp_genus = db.Column(db.String(144), nullable=False)
+    #sp_family = db.Column(db.String(144), nullable=False)
+    #sp_order = db.Column(db.String(144), nullable=False)
+    #description = db.Column(db.String(144), nullable=True)
 
-    def __init__(self, name, species, sp_genus, sp_family, sp_order, description):
+    #def __init__(self, name, species, sp_genus, sp_family, sp_order, description):
+    #def __init__(self, name):
         self.name = name
-        self.species = species
-        self.sp_genus = sp_genus
-        self.sp_family = sp_family
-        self.sp_order = sp_order
-        self.description = description
+        #self.species = species
+        #self.sp_genus = sp_genus
+        #self.sp_family = sp_family
+        #self.sp_order = sp_order
+        #self.description = description
