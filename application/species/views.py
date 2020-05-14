1aa1f477c5b7dfabe642e381f8c52abc910de6cc
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-14 19:55:48 +0300

    Add functionality for editing species information

--- application/species/views.py
+++ application/species/views.py
@@ -10,9 +10,29 @@ def species_index():
 def species_form():
     return render_template("species/new.html")
 
+@app.route("/species/<species_id>/", methods=["GET", "POST"])
+def species_edit_information(species_id):
+
+    species = Species.query.get(species_id)
+    
+    if request.method == "POST":
+        species.name = request.form.get("name")
+        species.species = request.form.get("species")
+        species.description = request.form.get("description")
+
+        db.session().commit()
+  
+        return redirect(url_for("species_index"))
+    return render_template("species/edit.html", species=species)
+
+
+    db.session().commit()
+  
+    return redirect(url_for("tasks_index"))
+
 @app.route("/species/", methods=["POST"])
 def species_create():
-    s = Species(request.form.get("name"), request.form.get("species"), 
+    s = Species(request.form.get("name"), request.form.get("species"),
     request.form.get("description"))
 
     db.session().add(s)
