2640530c4b7dff8b77aa9fbb0131d03f0f118f30
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-19 12:42:03 +0300

    Add fields in species creation form

--- application/species/forms.py
+++ application/species/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
+from wtforms import TextAreaField
 
 class SpeciesForm(FlaskForm):
-    name = StringField("Lajinimi, kansakielinen (esim. talitiainen)")
+    name = StringField("Lajinimi, kansakielinen (esim. talitiainen):")	
+    species = StringField("Lajinimi, tieteellinen (esim. Parus major):")
+    description = TextAreaField('Lajikuvaus:', render_kw={"rows": 10, "cols": 30})
  
     class Meta:
         csrf = False
