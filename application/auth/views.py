152ea0f2e7ed5932bb91d9be7c1b0220e6b7e3ef
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-20 13:26:01 +0300

    Add logout functionality

--- application/auth/views.py
+++ application/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 from application import app
 from application.auth.models import User
@@ -23,3 +23,10 @@ def auth_login():
 
     login_user(user)
     return redirect(url_for("index"))  
+
+
+
+@app.route("/auth/logout")
+def auth_logout():
+    logout_user()
+    return redirect(url_for("index"))    
