83d75a53003e3c54ccf950bb2e9d24418985b696
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-20 17:29:03 +0300

    Add functionality for registration

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,9 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, TextAreaField
   
 class LoginForm(FlaskForm):
-    username = StringField("Käyttäjätunnus")
-    password = PasswordField("Salasana")
+    username = StringField("Käyttäjätunnus:")
+    password = PasswordField("Salasana:")
+  
+    class Meta:
+        csrf = False
+
+class RegistrationForm(FlaskForm):
+    name = StringField("Nimi:")
+    username = StringField("Käyttäjätunnus:")
+    password = PasswordField("Salasana:")
+    info = TextAreaField("Lisätietoja:", render_kw={"rows": 10, "cols": 30})
   
     class Meta:
         csrf = False
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,36 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, RegistrationForm
+
+
+@app.route("/auth/register", methods = ["GET", "POST"])
+def auth_register():
+    if request.method == "GET":
+        return render_template("auth/registrationform.html", form = RegistrationForm())
+
+    form = RegistrationForm(request.form)
+
+    # validate here
+
+    username = User.query.filter_by(username=form.username.data).first()
+
+    if username:
+        return render_template("auth/registrationform.html", form = form,
+                               error = "Käyttäjätunnus on jo käytössä")
+    
+    user = User(form.name.data, form.username.data, form.password.data, form.info.data)
+
+    db.session().add(user)
+    db.session().commit()
+
+    user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
+
+    login_user(user)
+    return redirect(url_for("index"))  
+
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
