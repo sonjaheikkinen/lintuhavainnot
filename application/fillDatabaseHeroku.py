b23fd8b925f08953ad52df923042f5ec56c09081
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-22 14:07:41 +0300

    Change null coding from stmt

--- application/fillDatabaseHeroku.py
+++ application/fillDatabaseHeroku.py
@@ -3,5 +3,5 @@ from application.species.models import Species
 
 
 stmt = text("COPY species FROM 'application\species.txt'"
-            " USING DELIMITERS ';' WITH NULL AS '\NULL'")
+            " USING DELIMITERS ';' WITH NULL AS 'null'")
 db.engine.execute(stmt)
