6be3c79a75c05e8c5a48e5ec465f3e62aaba3b67
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-22 14:09:45 +0300

    Import text in fillDatabaseHeroku

--- application/fillDatabaseHeroku.py
+++ application/fillDatabaseHeroku.py
@@ -1,6 +1,6 @@
 from application import db
 from application.species.models import Species
-
+from sqlalchemy.sql import text
 
 stmt = text("COPY species FROM 'application\species.txt'"
             " USING DELIMITERS ';' WITH NULL AS 'null'")
