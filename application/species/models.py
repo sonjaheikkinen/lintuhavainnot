cfcc4df686f25367534c4546b010c7fccf55c48e
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-25 13:38:01 +0300

    Change species to Species

--- application/species/models.py
+++ application/species/models.py
@@ -9,7 +9,7 @@ class Species(db.Model):
     #sp_order = db.Column(db.String(144), nullable=False)
     description = db.Column(db.Text, nullable=True)
 
-    sightings = db.relationship("Sighting", backref='species', lazy=True)
+    sightings = db.relationship("Sighting", backref='Species', lazy=True)
 
     #def __init__(self, name, species, sp_genus, sp_family, sp_order, description):
     def __init__(self, name, species, description):
