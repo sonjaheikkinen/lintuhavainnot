64eb0eee60d419d4170874d5a1f129813936093b
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-25 16:10:44 +0300

    Add test user on init

--- application/fillDatabase.py
+++ application/fillDatabase.py
@@ -1,6 +1,11 @@
 from application import app, db
 from application.species.models import Species
+from application.auth.models import User
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt(app)
+
+# Lisataan lajeja
 speciesText = open("application/species.txt", "r")
 speciesList = []
 
@@ -13,4 +18,16 @@ speciesText.close()
 
 if not Species.query.all():
     db.session.add_all(speciesList)
+    db.session.commit()
+
+# Lisataan testikayttaja
+hashedPassword = bcrypt.generate_password_hash("testi").decode('utf-8')
+testUser = User("testi", "testi", hashedPassword, "Tämä on testikäyttäjä")
+userFound = User.query.filter_by(username="testi").first()
+if userFound:
+     if not bcrypt.check_password_hash(userFound.password, "testi"):
+         db.session.add(testUser)
+         db.session.commit()
+else: 
+    db.session.add(testUser)
     db.session.commit()
