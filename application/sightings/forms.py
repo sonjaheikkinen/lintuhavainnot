e4c14fc2d8be0bc718d0358f080e6213b8a1309b
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-25 13:28:55 +0300

    Add choicelist of all species to selectField

--- application/sightings/forms.py
+++ application/sightings/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, TextAreaField, validators
+from application.species.models import Species
 
 class AddSighting(FlaskForm):
-    species = SelectField("Valitse havaittu laji:", choices=[(1, "sinitiainen")], coerce=int)
+
+    species = Species.query.all()
+    choiceList = []
+    for species in species:
+        choiceList.append((species.id, species.name))
+
+    species = SelectField("Valitse havaittu laji:", choices=choiceList, coerce=int)
     info = TextAreaField("Lisätietoja havainnosta:", render_kw={"rows": 10, "cols": 30})
 
     class Meta:
