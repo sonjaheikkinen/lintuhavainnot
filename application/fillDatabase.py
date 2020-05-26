021ab516428d93156403b1878ce7b5ff8c3827f4
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-26 13:51:59 +0300

    Create directory files and remove testuser information from code

--- application/fillDatabase.py
+++ application/fillDatabase.py
@@ -1,12 +1,9 @@
 from application import app, db
 from application.species.models import Species
 from application.auth.models import User
-from flask_bcrypt import Bcrypt
-
-bcrypt = Bcrypt(app)
 
 # Lisataan lajeja
-speciesText = open("application/species.txt", "r")
+speciesText = open("application/files/species.txt", "r")
 speciesList = []
 
 for line in speciesText:
@@ -21,13 +18,8 @@ if not Species.query.all():
     db.session.commit()
 
 # Lisataan testikayttaja
-hashedPassword = bcrypt.generate_password_hash("testi").decode('utf-8')
-testUser = User("testi", "testi", hashedPassword, "Tämä on testikäyttäjä")
-userFound = User.query.filter_by(username="testi").first()
-if userFound:
-     if not bcrypt.check_password_hash(userFound.password, "testi"):
-         db.session.add(testUser)
-         db.session.commit()
-else: 
-    db.session.add(testUser)
+attributes = open("application/files/users.txt", "r").split(";")
+user = User(attributes[0], attributes[1], attributes[2], attributes[3])
+if not User.query.all():
+    db.session.add(user)
     db.session.commit()
--- application/fillDatabaseHeroku.py
+++
@@ -1,7 +0,0 @@
-from application import db
-from application.species.models import Species
-from sqlalchemy.sql import text
-
-stmt = text("COPY species FROM 'application\species.txt'"
-            " USING DELIMITERS ';' WITH NULL AS 'null'")
-db.engine.execute(stmt)
