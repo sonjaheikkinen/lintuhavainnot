8f832930642680090383d1437b11e34d0a71dc2d
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-05-26 14:16:34 +0300

    Add date information to species

--- application/species/models.py
+++ application/species/models.py
@@ -2,6 +2,9 @@ from application import db
 
 class Species(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+                              onupdate=db.func.current_timestamp())
     name = db.Column(db.String(255), nullable=False)
     species = db.Column(db.String(255), nullable=False)
     #sp_genus = db.Column(db.String(144), nullable=False)
