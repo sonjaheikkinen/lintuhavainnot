bcba95ebd34a2b3fab464e8b02c241e8d5be5b9a
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-06-01 20:50:20 +0300

    Use bootstrap on all sightings forms and tables

--- application/sightings/forms.py
+++ application/sightings/forms.py
@@ -2,15 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, SelectMultipleField, TextAreaField, validators, widgets
 from application.species.models import Species
 
-class MultiCheckboxField(SelectMultipleField):
-    widget = widgets.ListWidget(prefix_label=False)
-    option_widget = widgets.CheckboxInput()
-
 class AddSighting(FlaskForm):
 
     species = SelectField("Valitse havaittu laji:", coerce=int)
     place = StringField("Havaintopaikka:", [validators.Length(min=1, max=255, message="Kentän tulee sisältää vähintään %(min)d ja enintään %(max)d merkkiä.")])
-    habitats = MultiCheckboxField("Valitse elinympäristöt:", coerce=int)
+    habitats = SelectMultipleField("Valitse elinympäristöt (pidä Ctrl-näppäin pohjassa valitaksesi useampia vaihtoehtoja):", coerce=int)
     info = TextAreaField("Lisätietoja havainnosta:", render_kw={"rows": 10, "cols": 30})
 
     class Meta:
