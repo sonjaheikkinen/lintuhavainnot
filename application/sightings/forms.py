000c28b1431d5cb689c047c2740912c82d5f9fe8
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-06-08 22:05:52 +0300

    Edit new sighting form label

--- application/sightings/forms.py
+++ application/sightings/forms.py
@@ -6,7 +6,7 @@ class AddSighting(FlaskForm):
 
     species = SelectField("Valitse havaittu laji:", coerce=int)
     place = StringField("Havaintopaikka:", [validators.Length(min=1, max=255, message="Kentän tulee sisältää vähintään %(min)d ja enintään %(max)d merkkiä.")])
-    habitats = SelectMultipleField("Valitse elinympäristöt (pidä Ctrl-näppäin pohjassa valitaksesi useampia vaihtoehtoja):", coerce=int)
+    habitats = SelectMultipleField("Minkälainen ympäristö havaintopaikassa oli? (pidä Ctrl-näppäin pohjassa valitaksesi useampia vaihtoehtoja):", coerce=int)
     info = TextAreaField("Lisätietoja havainnosta:", render_kw={"rows": 10, "cols": 30})
 
     class Meta:
